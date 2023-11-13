41037e3ee985884aad87059b18c868fe351ef475
Morgan Berger <morgan.berger71@gmail.com>
2023-11-13 17:31:54 +0100

    chore: ios-example
    Adds first draft for ios-example app.
    Only with UIKit so far.

---
+++ ios-example/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  ios-example
+//
+//  Created by Morgan Berger on 13/11/2023.
+//
+
+import UIKit
+import AccessIOS
+import AppTrackingTransparency
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var loremLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Access.config(["debug": true])
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Access.clearCookies()
+        
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { [weak self] _ in
+                self?.instanciateAccess()
+                self?.setupAccessEvents()
+                self?.createPaywall()
+            }
+        }
+    }
+    
+    private func instanciateAccess() {
+        try? Access.instanciate(key: "<your_app_id_here>")
+    }
+    
+    private func setupAccessEvents() {
+        Access.onLock {
+            // content was locked
+        }
+        Access.onReady { _ in
+            // paywall is ready
+        }
+        Access.onRelease { _ in
+            // paywall was released
+        }
+    }
+    
+    private func createPaywall() {
+        Access.createPaywall(pageType: "premium", view: contentView, percent: 90) {
+            // Paywall was completed
+        }
+    }
+}
+
